e2f67ad2bf23e98123d0812432f9994796fd27d7
TimProrok <111649612+TimProrok@users.noreply.github.com>
2022-08-20 10:54:41 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by Tim Prorok on 8/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Tim!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
